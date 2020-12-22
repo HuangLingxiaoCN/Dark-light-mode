e0970b920cf4e5123ab74e9e2a2f1e5692ce917c
Jason Huang <jasonhlx1213@gmail.com>
2020-12-22 23:43:24 +0200

    mobile responsive design

--- script.js
+++ script.js
@@ -50,3 +50,13 @@ if (currentTheme) {
     toggleMode(true);
   }
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// function myFunction() {
+//   var x = document.querySelector('.icon');
+//   if (x.className === "nav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "nav";
+//   }
+// }
